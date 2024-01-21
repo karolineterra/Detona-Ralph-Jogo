bf26778c7a899a537f5bf71b507203215d8dcbaa
karolineterra <terra.karoline1@gmail.com>
2024-01-21 00:49:58 -0300

    listene de hitbox

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -7,8 +7,10 @@ const state = {
     },
     values: {
         timerId: null,
-        gameVelocity: 1000
-    }
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+    },
 };
 
 function randomSquare(){
@@ -19,19 +21,28 @@ function randomSquare(){
     let randomNumber = Math.floor(Math.random()*9);
     let randomSquare = state.view.squares[randomNumber];
     randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
 }
 
 function moveEnemy(){
     state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
 }
 
-function addListenerHitbox() {
+function addListenerHitBox() {
     state.view.squares.forEach((square) => {
-    })
+        square.addEventListener("mousedown", () => {
+            if(square.id === state.values.hitPosition) {
+                state.values.result++
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+            }
+        });
+    });
 }
 
 function initialize(){
     moveEnemy();
+    addListenerHitBox();
 }
 
 initialize();
